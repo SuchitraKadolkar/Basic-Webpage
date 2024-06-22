f0d46e719bf6faca2b4d6c167c23800b15585809
Suchitra Kadolkar <62543141+SuchitraKadolkar@users.noreply.github.com>
2024-06-22 21:29:01 +0530

    Update solution.py

--- Problem2/solution.py
+++ Problem2/solution.py
@@ -1,1 +1,12 @@
-n = input()
+arr = input()
+n = len(arr)
+farthest = 0
+planes_cnt = 0
+next_step = 0
+        
+for i in range(n - 1):
+  farthest = max(i + nums[i], farthest)
+  if i == next_step:
+    next_step = farthest
+    planes_cnt += 1    
+return planes_cnt
